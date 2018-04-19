af4e61f26ed2fb0040f9e1308fef8c18592f7787
Mohamed Ragab <33547443+mohamedragab33@users.noreply.github.com>
2018-04-19 16:21:36 +0200

    Add files via upload

---
+++ WindowsFormsApplication6/WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txttype = new System.Windows.Forms.TextBox();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.last = new System.Windows.Forms.Button();
+            this.front = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.first = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber.Location = new System.Drawing.Point(220, 307);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(631, 34);
+            this.txtNumber.TabIndex = 0;
+            // 
+            // txttype
+            // 
+            this.txttype.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttype.Location = new System.Drawing.Point(220, 235);
+            this.txttype.Name = "txttype";
+            this.txttype.Size = new System.Drawing.Size(631, 34);
+            this.txttype.TabIndex = 1;
+            // 
+            // txtid
+            // 
+            this.txtid.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtid.Location = new System.Drawing.Point(220, 163);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(631, 34);
+            this.txtid.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(196, 520);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 304);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Car Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Car Type";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Car ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Turquoise;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(195, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(698, 112);
+            this.panel2.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(114, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(444, 35);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "YOUR TASK IS BE POSSIBLE";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // last
+            // 
+            this.last.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.last.Location = new System.Drawing.Point(233, 447);
+            this.last.Name = "last";
+            this.last.Size = new System.Drawing.Size(102, 43);
+            this.last.TabIndex = 5;
+            this.last.Text = "<<";
+            this.last.UseVisualStyleBackColor = true;
+            this.last.Click += new System.EventHandler(this.last_Click);
+            // 
+            // front
+            // 
+            this.front.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.front.Location = new System.Drawing.Point(352, 447);
+            this.front.Name = "front";
+            this.front.Size = new System.Drawing.Size(102, 43);
+            this.front.TabIndex = 6;
+            this.front.Text = "<";
+            this.front.UseVisualStyleBackColor = true;
+            this.front.Click += new System.EventHandler(this.front_Click);
+            // 
+            // next
+            // 
+            this.next.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next.Location = new System.Drawing.Point(605, 447);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(102, 43);
+            this.next.TabIndex = 7;
+            this.next.Text = ">";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // first
+            // 
+            this.first.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.first.Location = new System.Drawing.Point(742, 447);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(102, 43);
+            this.first.TabIndex = 8;
+            this.first.Text = ">>";
+            this.first.UseVisualStyleBackColor = true;
+            this.first.Click += new System.EventHandler(this.first_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(491, 452);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "position";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(397, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 43);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(261, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 43);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "NEW";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(672, 364);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 43);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(535, 364);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 43);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(893, 520);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.first);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.front);
+            this.Controls.Add(this.last);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.txttype);
+            this.Controls.Add(this.txtNumber);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txttype;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button last;
+        private System.Windows.Forms.Button front;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button first;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication6/WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace WindowsFormsApplication6
+{ 
+  
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection("Server=DESKTOP-C7AP5JN; DataBase=car; Integrated Security =true ; ");
+        SqlDataAdapter da;
+        DataTable datatable = new DataTable();
+        CurrencyManager cm;
+        SqlCommandBuilder commandbuilder;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            da = new SqlDataAdapter("Select * From Car1",cn);
+            da.Fill(datatable);
+            txtid.DataBindings.Add("Text",datatable,"CID");
+            txtNumber.DataBindings.Add("Text", datatable, "cNumber");
+            txttype.DataBindings.Add("Text",datatable,"CType");
+            cm = (CurrencyManager)this.BindingContext[datatable];
+            
+            label4.Text = (cm.Position + 1) + " / " + datatable.Rows.Count;
+
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void last_Click(object sender, EventArgs e)
+        {
+            cm.Position = 0;
+            label4.Text = (cm.Position + 1) + " / " + datatable.Rows.Count;
+        }
+
+        private void front_Click(object sender, EventArgs e)
+        {
+            cm.Position -=1 ;
+            label4.Text = (cm.Position + 1) + " / " + datatable.Rows.Count;
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            cm.Position += 1;
+            label4.Text = (cm.Position + 1) + " / " + datatable.Rows.Count;
+        }
+
+        private void first_Click(object sender, EventArgs e)
+        {
+            cm.Position = datatable.Rows.Count - 1;
+            label4.Text = (cm.Position + 1) + " / " + datatable.Rows.Count;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+              
+             cm.AddNew();
+              txtid.Focus();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cm.EndCurrentEdit();
+            commandbuilder = new SqlCommandBuilder(da);
+            da.Update(datatable);
+            MessageBox.Show("ADDED ","Addtion");
+            label4.Text = (cm.Position + 1) + " / " + datatable.Rows.Count;
+
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cm.RemoveAt(cm.Position);
+            cm.EndCurrentEdit();
+            commandbuilder = new SqlCommandBuilder(da);
+            da.Update(datatable);
+            MessageBox.Show("delted ", "delte");
+            label4.Text = (cm.Position + 1) + " / " + datatable.Rows.Count;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cm.EndCurrentEdit();
+            commandbuilder = new SqlCommandBuilder(da);
+            da.Update(datatable);
+            cm.Refresh();
+            MessageBox.Show("update ", "update");
+            label4.Text = (cm.Position + 1) + " / " + datatable.Rows.Count;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
